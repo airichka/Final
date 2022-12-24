0101ea246ed0223fe5b1f4c22494c26bb0454a00
Irina.TA <Irina.TA@gmail.com>
2022-12-24 18:23:28 +0400

    Сбор и парсинг слов

---
+++ Final_Task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите через запитую элементы массива: ");
+var userInput = Console.ReadLine();
+
+var words = ParseWords(userInput);
+
+
+IEnumerable<string> ParseWords(string userInput)
+{
+	return userInput.Replace(" ", string.Empty).Split(',');
+}

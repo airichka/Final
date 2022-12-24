55b14c6e0e254911674c9fca778cb10a70e76ae1
Irina.TA <Irina.TA@gmail.com>
2022-12-24 18:26:15 +0400

    Вывод слов тольк с 3мя символами

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -2,9 +2,19 @@
 var userInput = Console.ReadLine();
 
 var words = ParseWords(userInput);
+var wordsWhith3Chars = GetWordsWith3Chars(words);
 
+foreach (var word in wordsWhith3Chars)
+{
+	Console.WriteLine(word);
+}
 
 IEnumerable<string> ParseWords(string userInput)
 {
 	return userInput.Replace(" ", string.Empty).Split(',');
+}
+
+IEnumerable<string> GetWordsWith3Chars(IEnumerable<string> strings)
+{
+	return strings.Where(x => x.Length <= 3);
 }
